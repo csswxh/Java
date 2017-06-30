11e7755d5ce87d9bb9075dc05c4f84b9a06b3fac
csswxh <772948924@qq.com>
2017-06-30 16:26:06 +0800

    xs

---
+++ src/com/css/java/Java.java
@@ -0,0 +1,24 @@
+/**
+ * <b>项目名：</b>Java<br/>  
+ * <b>包名：</b>com.css.java<br/>  
+ * <b>文件名：</b>Java.java<br/>  
+ * <b>版本信息：</b>1.0<br/>  
+ * <b>日期：</b>2017年6月30日 下午4:24:58<br/>  
+ * <b>COPYRIGHT 2010-2012 ALL RIGHTS RESERVED 中国软件与技术服务股份有限公司</b>-版权所有<br/>
+ */
+package com.css.java;
+/**
+ * @description TODO
+ * @createTime 2017年6月30日 下午4:24:58
+ * @modifyTime 
+ * @author wangxianhui
+ * @version 1.0
+ */
+public class Java {
+
+	public static void main(String[] args) {
+		System.out.println("java");
+
+	}
+
+}
